bdddcaeccb76a1017e6acd6aac7d3ab14ccd0289
TTT-72 <ablhole998877@gmail.com>
2025-07-04 14:20:04 +0900

    add cautionary comment

--- 05_Basicset_BLE/05_BLEkeyboard/05_BLEkeyboard.ino
+++ 05_Basicset_BLE/05_BLEkeyboard/05_BLEkeyboard.ino
@@ -4,10 +4,11 @@
 // https://github.com/T-vK/ESP32-BLE-Keyboard
 // Remove comment out #define USE_NIMBLE
 // Install NimBLE-Arduino library
+// ※Choose NimBLE-Arduino ver,1.x 
 
 #define swPin 3 //3:ConnectorA 4:ConnectorB
 
-BleKeyboard bleKeyboard("ESP32 Keyboard"); // Can be renamed to any device name you like.
+BleKeyboard bleKeyboard("TTT ESP32 Keyboard"); // Can be renamed to any device name you like.
 
 void setup() {
   bleKeyboard.begin();
